43b164b73e7191fff8ec4c6db8086c200a3870e9
divya singireddy <divyasingireddy95@gmail.com>
2024-08-07 15:05:28 +0530

    variables

--- 01-variables.py
+++ 01-variables.py
@@ -10,3 +10,18 @@ a = 42
 # Print the value that is stored inside a variable 
 print(a)
 print ("this is my python code")
+
+# Float value
+b = 42.345
+
+# Boolean value
+c = True
+
+# Strings
+d = 'Divya Reddy'
+e = "My Husband's name is Srikanth Reddy"
+f = """
+ This is a multi-line string
+"""
+
+print(d)
