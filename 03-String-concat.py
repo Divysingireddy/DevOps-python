30f349fd31053a07470005a0c7026e5df61b1a94
divya singireddy <divyasingireddy95@gmail.com>
2024-08-09 06:47:56 +0530

    string functions

--- 03-String-concat.py
+++ 03-String-concat.py
@@ -9,3 +9,10 @@ print(result)
 str3 = "My name is Divya Reddy"
 length = len(str3)
 print("Length of the name is:", length)
+
+# Conversion to Upper and lower
+
+uppercase = str3.upper()
+lowercase = str3.lower()
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
