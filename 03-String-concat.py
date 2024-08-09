5888f46478c800ff975f86fcdea66c9226409edd
divya singireddy <divyasingireddy95@gmail.com>
2024-08-09 06:44:52 +0530

    string functions

--- 03-String-concat.py
+++ 03-String-concat.py
@@ -1,4 +1,11 @@
+# Concatination function
 str1 = "Bhavish"
 str2 = "Reddy"
 result = str1 + " " + str2
 print(result)
+
+# Length of a String
+
+str3 = "My name is Divya Reddy"
+length = len(str3)
+print("Length of the name is:", length)
